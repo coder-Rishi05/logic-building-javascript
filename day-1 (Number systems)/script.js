31b5fa7ba5998696d856ad62b7ba89bb1400267a
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-01-22 10:25:08 +0530

    solved by 2 ways

--- day-1 (Number systems)/script.js
+++ day-1 (Number systems)/script.js
@@ -1,4 +1,9 @@
+// for (let j = 1; j <= 100; j++) {
+//   let num = (j - 101).toFixed().split("-")[1];
+//   console.log(Number(num));
+// }
+
+let a = 101;
 for (let j = 1; j <= 100; j++) {
-  let num = (j - 101).toFixed().split("-")[1];
-  console.log(Number(num));
+  console.log(a - j);
 }
--- day-1 (Number systems)/sol.md
+++ day-1 (Number systems)/sol.md
@@ -37,11 +37,18 @@ p.innerHTML += i + "<br>";
 
 ### q2
 
+- 1st type
 ```js
 for (let j = 1; j <= 100; j++) {
   let num = (j - 101).toFixed().split("-")[1];
   console.log(Number(num));
 }
 
+- 2nd type
+
+let a = 101;
+for (let j = 1; j <= 100; j++) {
+  console.log(a - j);
+}
 
 ```
